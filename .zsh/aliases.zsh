de11ec5bb65eeef17b585edc68d7b71391b950db
Bree Elle Gardner <letsbreelhere@gmail.com>
2021-09-07 14:38:07 -0700

    Thing the thing

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -6,3 +6,10 @@ alias ag='rg'
 alias glint='eslint `git diff master --name-only`'
 alias pretty='prettier --write `git diff master --name-only`'
 alias killbg='kill -9 ${${(v)jobstates##*:*:}%=*}'
+alias restartnetwork='sudo nmcli con up "Wired connection 1"'
+
+# surfshark vpn aliases
+alias ssv="sudo surfshark-vpn"
+alias ssvs="sudo surfshark-vpn status"
+alias ssva="sudo surfshark-vpn attack"
+alias ssvd="sudo surfshark-vpn down"
