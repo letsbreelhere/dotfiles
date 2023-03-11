28113f15702104c94b4d6ba26bf5c32b78d0ae4a
letsbreelhere <letsbreelhere@gmail.com>
2023-03-10 17:05:51 -0800

    Aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,13 +1,11 @@
 alias vim=nvim
-alias vi=nvim
 alias be='bundle exec'
 alias rspec='bundle exec rspec'
 alias g=git
 alias glint='eslint `git diff master --name-only`'
 alias pretty='prettier --write `git diff master --name-only`'
 alias killbg='kill -9 ${${(v)jobstates##*:*:}%=*}'
-alias dra='dev run app'
-
-function col() {
-  awk "{print \$$1}"
-}
+alias pbcopy='xclip -selection clipboard'
+alias pbpaste='xclip -selection clipboard -o'
+alias ls='ls --color=auto'
+alias open=xdg-open
